fff8e54dc65a027975ada5751b7a2e42e788de76
edilson <edilsondiasgomes@gmail.com>
2025-07-19 08:30:29 -0300

    acrescentando o bucket no cors

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 
 const corsOptions = {
-  origin: ['https://rentals-frontend-nine.vercel.app','rentals-frontend-nine.vercel.app', 'http://localhost:4200'],
+  origin: ['https://rentals-frontend-nine.vercel.app','rentals-frontend-nine.vercel.app', 'http://localhost:4200', 'https://storage.googleapis.com'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
